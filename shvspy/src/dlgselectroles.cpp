41b9ce67559839f99b272dee9a0a8c84ffa01261
Martin Špirk <mspirk@elektroline.cz>
2026-09-24 14:08:01 +0200

    edit order of roles in role assignment

--- shvspy/src/dlgselectroles.cpp
+++ shvspy/src/dlgselectroles.cpp
@@ -9,6 +9,13 @@ DlgSelectRoles::DlgSelectRoles(QWidget *parent):
 	ui(new Ui::DlgSelectRoles)
 {
 	ui->setupUi(this);
+	ui->splitter->setStretchFactor(0, 2);
+	ui->splitter->setStretchFactor(1, 1);
+
+	connect(ui->tbMoveRoleUp, &QToolButton::clicked, this, [this]() { moveSelectedRole(-1); });
+	connect(ui->tbMoveRoleDown, &QToolButton::clicked, this, [this]() { moveSelectedRole(1); });
+	connect(ui->lstSelectedRoles, &QListWidget::currentRowChanged, this, &DlgSelectRoles::updateMoveButtons);
+	updateMoveButtons();
 }
 
 void DlgSelectRoles::init(shv::iotqt::rpc::ClientConnection *rpc_connection, const std::string &acl_etc_node_path, const QStringList &roles)
@@ -19,15 +26,16 @@ void DlgSelectRoles::init(shv::iotqt::rpc::ClientConnection *rpc_connection, con
 	m_rolesTreeModel = new RolesTreeModel(this);
 	m_rolesTreeModel->load(rpc_connection, aclEtcRolesNodePath());
 	ui->tvRoles->setModel(m_rolesTreeModel);
-	m_userRoles = roles;
+	ui->lstSelectedRoles->addItems(roles);
 
 	ui->lblStatus->setText(tr("Loading..."));
 	ui->tvRoles->setEnabled(false);
 
-	connect(m_rolesTreeModel, &RolesTreeModel::loadFinished, this, [this](){
+	connect(m_rolesTreeModel, &RolesTreeModel::loadFinished, this, [this, roles](){
 		ui->lblStatus->clear();
+		m_rolesTreeModel->setSelectedRoles(roles);
 		ui->tvRoles->setEnabled(true);
-		m_rolesTreeModel->setSelectedRoles(m_userRoles);
+		syncSelectedRolesList();
 
 		if (!m_currentItemPath.isEmpty()) {
 			QStandardItem *item = findChildItem(m_rolesTreeModel->invisibleRootItem(), m_currentItemPath);
@@ -39,6 +47,8 @@ void DlgSelectRoles::init(shv::iotqt::rpc::ClientConnection *rpc_connection, con
 		}
 	});
 
+	connect(m_rolesTreeModel, &QStandardItemModel::itemChanged, this, &DlgSelectRoles::syncSelectedRolesList);
+
 	connect(m_rolesTreeModel, &RolesTreeModel::loadError, this, [this](QString error){
 		ui->lblStatus->setText(error);
 	});
@@ -46,12 +56,52 @@ void DlgSelectRoles::init(shv::iotqt::rpc::ClientConnection *rpc_connection, con
 
 QStringList DlgSelectRoles::selectedRoles()
 {
-	return m_rolesTreeModel->selectedRoles();
+	QStringList roles;
+	for (int i = 0; i < ui->lstSelectedRoles->count(); ++i) {
+		roles << ui->lstSelectedRoles->item(i)->text();
+	}
+	return roles;
 }
 
-void DlgSelectRoles::setUserRoles(const QStringList &roles)
+void DlgSelectRoles::syncSelectedRolesList()
 {
-	m_rolesTreeModel->setSelectedRoles(roles);
+	if (!ui->tvRoles->isEnabled()) {  //do nothing while model loading
+		return;
+	}
+
+	const QStringList checked_roles = m_rolesTreeModel->selectedRoles();
+
+	for (auto i = ui->lstSelectedRoles->count() - 1; i >= 0; --i) {
+		const QString &role = ui->lstSelectedRoles->item(i)->text();
+		if (!checked_roles.contains(role)) {
+			delete ui->lstSelectedRoles->takeItem(i);
+		}
+	}
+	auto already_selected_roles = selectedRoles();
+	for (const auto &role : checked_roles) {
+		if (!already_selected_roles.contains(role)) {
+			ui->lstSelectedRoles->addItem(role);
+		}
+	}
+	updateMoveButtons();
+}
+
+void DlgSelectRoles::moveSelectedRole(int offset)
+{
+	int row = ui->lstSelectedRoles->currentRow();
+	int new_row = row + offset;
+	if (row < 0 || new_row < 0 || new_row >= ui->lstSelectedRoles->count()) {
+		return;
+	}
+	ui->lstSelectedRoles->insertItem(new_row, ui->lstSelectedRoles->takeItem(row));
+	ui->lstSelectedRoles->setCurrentRow(new_row);
+}
+
+void DlgSelectRoles::updateMoveButtons()
+{
+	int row = ui->lstSelectedRoles->currentRow();
+	ui->tbMoveRoleUp->setEnabled(row > 0);
+	ui->tbMoveRoleDown->setEnabled(row >= 0 && row < ui->lstSelectedRoles->count() - 1);
 }
 
 QStandardItem *DlgSelectRoles::findChildItem(QStandardItem *item, const QStringList &path, int ix)
